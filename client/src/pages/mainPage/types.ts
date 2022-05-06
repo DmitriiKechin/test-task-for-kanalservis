e46626d8c9ae513c3a4ee5601c44f7d8ef865312
Dmitrii Kechin <dmitrii.kechin@gmail.com>
2022-05-06 18:57:28 +0300

    deleted file

--- client/src/pages/mainPage/types.ts
+++
@@ -1,9 +0,0 @@
-import { SerializedError } from '@reduxjs/toolkit';
-import { FetchBaseQueryError } from '@reduxjs/toolkit/dist/query';
-import { IOrder } from '../../models/IOrder';
-
-export interface IFetchOrder {
-  orders: IOrder[] | undefined;
-  isLoading: boolean;
-  error: FetchBaseQueryError | SerializedError | undefined;
-}
