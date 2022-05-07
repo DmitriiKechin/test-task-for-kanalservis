a96920d0740707a1fe14c50c371883d32dfaa88d
Dmitrii Kechin <dmitrii.kechin@gmail.com>
2022-05-07 11:54:17 +0300

    added padding

--- client/src/pages/mainPage/styles.ts
+++ client/src/pages/mainPage/styles.ts
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   height: 100%;
 
+  padding-top: 10rem;
+
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
 `;
